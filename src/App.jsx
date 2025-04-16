e28d772e15ff80ba085475ce79c1e3d86bc129fc
andrii <a.iakumiv@gmail.com>
2025-04-16 08:10:13 +0300

    Add ChangeHeading component to update document title on button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import UseEffectHook from './components/useEffectHook/useEffectHook';
+import ChangeHeading from './components/changeHeading/changeHeading';
 
 const App = () => {
     return (
         <>
             <UseEffectHook />
+            <hr />
+            <ChangeHeading />
+            <hr />
+            
         </>
     );
 };
