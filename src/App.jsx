886faebe70986ec39a8867fed20179a9933e4300
andrii <a.iakumiv@gmail.com>
2025-04-17 08:35:29 +0300

    Refactor App component to include main application structure and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import UseEffectHook from './components/useEffectHook/useEffectHook';
+import ChangeHeading from './components/changeHeading/changeHeading';
+import DisassemblyStage from './components/DisassemblyStage/DisassemblyStage';
+import UpdateStage from './components/UpdateStage/UpdateStage';
 
 const App = () => {
     return (
         <>
-            
+            <UseEffectHook />
+            <hr />
+            <ChangeHeading />
+            <hr />
+            <DisassemblyStage/>
+            <hr />
+            <UpdateStage/>
         </>
     );
 };
